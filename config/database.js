df32c50c3a955eb1d881ab93d95639385396e56b
Dave <dweight13@hotnail.com>
2018-05-24 12:41:19 +0800

    db

--- config/database.js
+++ config/database.js
@@ -1,12 +1,12 @@
-if (process.env.NODE_ENV === 'production') {
+//if (process.env.NODE_ENV === 'production') {
   module.exports = { 
     database: 'mongodb://Dave:1234@ds131697.mlab.com:31697/UPUSS',
     secret: 'secret_key' 
   } //External db
-}
-else {
-  module.exports = { 
-    database: 'mongodb://localhost/UPUSS',
-    secret: 'secret_key' 
-   } //local db
-}
+//}
+//else {
+//  module.exports = { 
+ //   database: 'mongodb://localhost/UPUSS',
+  //  secret: 'secret_key' 
+  // } //local db
+//}
