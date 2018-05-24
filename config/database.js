b19b0160d32dee02a44fb5698068ba975e1ae18a
Dave <dweight13@hotnail.com>
2018-05-24 17:42:41 +0800

    db fix

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 if (process.env.NODE_ENV === 'production') {
   module.exports = { 
-    database: 'mongodb://Dave:1234@ds131697.mlab.com:31697/UPUSS',
+    database: 'mongodb://calis13:bison1812@ds151259.mlab.com:51259/upuss',
     secret: 'secret_key' 
   } //External db
 }
