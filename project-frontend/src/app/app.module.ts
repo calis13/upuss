f7b64b1cb3467e7caeaaba9b68707d64788b6839
Dave <dweight13@hotnail.com>
2018-06-01 13:39:33 +0800

    fix

--- project-frontend/src/app/app.module.ts
+++ project-frontend/src/app/app.module.ts
@@ -86,7 +86,7 @@ const routes: Routes = [
       }
     })
   ],
-  providers: [ValidateService, AuthService, AuthGuard, PusherService, GameService],
+  providers: [ValidateService, AuthService, AuthGuard, PusherService, GamesService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
